3fae2059ff05152bf5871c1dd66dc41c12850c50
tycoon87 <tyson.woodruff@outlook.com>
2017-08-24 18:42:26 -0700

    It Works

--- Users.js
+++ Users.js
@@ -1,7 +1,15 @@
 $('form').submit(function(){
-        $("#UserInfo").append("<div class='Last'>"+$("#last").val()+ "</div><div class='First'>"+$("#first").val()+ "</div><div class='Email'>"+$("#email").val()+ "</div><div class='Contact'>"+$("#contact").val()+ "</div>");
-        console.log($("#first").val());
-        console.log ('click');
+        
+        var temp1 = $('#last').val();
+//        $("#lastN").append(temp1);
+        var temp2 = $('#first').val();
+  
+//        $('#firstN').append(temp2);
+        var temp3 = $('#email').val();
+//        $('#emailT').append(temp3);
+        var temp4 = $('#contact').val();
+//        $('#phone').append(temp4);
+        $('tbody').append( '<tr><td>' + temp1 + '</td><td>' +temp2+ '</td><td>' +temp3+ '</td><td>' +temp4+ '</td></tr>')
+    
         return false;
 })
- $("UserInfo").append()
